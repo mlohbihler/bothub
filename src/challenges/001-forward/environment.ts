c648012baaaf679756fd8b6d500596d512314a99
Matthew Lohbihler <matthew@serotonin.ai>
2024-09-26 13:37:57 -0400

    goal disable animation

--- src/challenges/001-forward/environment.ts
+++ src/challenges/001-forward/environment.ts
@@ -6,18 +6,13 @@ import Controller from '../controller'
 export default class Environment implements IEnvironment {
   updatables: Steppable[] = []
   #agent
-
-  goalCenter = Vec2(300, 0)
-  goalDistanceSq = Math.pow(30 - radius, 2)
-
-  timestamp = 0
-  goalDisabled = false
-  goalAchieved = false
+  goal
 
   constructor(world: World, controller: Controller) {
     const agent = new Physiology(world, controller)
     this.#agent = agent
-    this.updatables.push(agent)
+    this.goal = new Goal(agent)
+    this.updatables.push(agent, this.goal)
   }
 
   getAgent() {
@@ -26,10 +21,31 @@ export default class Environment implements IEnvironment {
 
   step(evt: StepEvent) {
     this.updatables.forEach(e => e.step(evt))
+  }
 
+  render(cx: CanvasRenderingContext2D) {
+    this.goal.render(cx)
+  }
+}
+
+class Goal implements Steppable {
+  agent
+  goalCenter = Vec2(300, 0)
+  goalDistanceSq = Math.pow(30 - radius, 2)
+
+  constructor(agent: Physiology) {
+    this.agent = agent
+  }
+
+  timestamp = 0
+  goalDisabled = false
+  goalAchieved = false
+  disableRadius: number | undefined
+
+  step(evt: StepEvent) {
     this.timestamp = evt.timestamp
     this.goalDisabled ||= !!evt.keyEvents.length
-    this.goalAchieved ||= Vec2.distanceSquared(this.goalCenter, this.#agent.bug.getPosition()) < this.goalDistanceSq
+    this.goalAchieved ||= Vec2.distanceSquared(this.goalCenter, this.agent.bug.getPosition()) < this.goalDistanceSq
   }
 
   render(cx: CanvasRenderingContext2D) {
@@ -38,7 +54,14 @@ export default class Environment implements IEnvironment {
     if (this.goalDisabled) {
       cx.fillStyle = '#8882'
       cx.strokeStyle = '#8888'
-      radius = 30
+      if (this.disableRadius === undefined) {
+        this.disableRadius = radius
+      } else {
+        if (this.disableRadius > 0) {
+          this.disableRadius -= 0.35
+        }
+        radius = this.disableRadius
+      }
     } else if (this.goalAchieved) {
       cx.fillStyle = '#0f02'
       cx.strokeStyle = '#0f08'
@@ -46,11 +69,15 @@ export default class Environment implements IEnvironment {
       cx.fillStyle = '#ff01'
       cx.strokeStyle = '#ff08'
     }
-    cx.lineWidth = 1.5
 
-    cx.beginPath()
-    cx.ellipse(300, 0, radius, radius, 0, 0, Math.PI * 2)
-    cx.fill()
-    cx.stroke()
+    if (radius > 0) {
+      cx.lineWidth = 1.5
+
+      cx.beginPath()
+      cx.ellipse(300, 0, radius, radius, 0, 0, Math.PI * 2)
+      cx.fill()
+      cx.stroke()
+    } else {
+    }
   }
 }
